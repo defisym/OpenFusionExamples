ccb96926183e12b4e647150d44808328e24b2f76
Defisym <55148235+defisym@users.noreply.github.com>
2021-08-19 17:31:50 +0800

    GetFilterID by file ext defined in filepath
    Clean up
    GetFilterID by file ext defined in filepath

--- Extensions/WinAPI/Main.cpp
+++ Extensions/WinAPI/Main.cpp
@@ -354,82 +354,168 @@ short WINAPI DLLExport GoWindowed(LPRDATA rdPtr, long param1, long param2) {
 	return 0;
 }
 
+////截取场景区域到剪贴板
+//short WINAPI DLLExport BitBltFrameArea(LPRDATA rdPtr, long param1, long param2) {
+//	// 截取
+//	// 参考范例：
+//	// https://docs.microsoft.com/en-us/windows/win32/gdi/capturing-an-image
+//
+//	// Source Area
+//	RECT CurrentFrameRect;
+//	::GetWindowRect(rdPtr->FrameWindowHandle, &CurrentFrameRect);
+//	int FrameWidth = CurrentFrameRect.right - CurrentFrameRect.left;
+//	int FrameHeight = CurrentFrameRect.bottom - CurrentFrameRect.top;
+//
+//	// Defination
+//	HDC hdcWindow;
+//	HDC hdcMemDC = NULL;
+//	HBITMAP hbmScreen = NULL;
+//
+//	// Retrieve the handle to a display device context.
+//	hdcWindow = GetDC(rdPtr->FrameWindowHandle);
+//
+//	// Create a compatible DC, which is used in a BitBlt from the window DC.
+//	hdcMemDC = CreateCompatibleDC(hdcWindow);
+//
+//	// Create a compatible bitmap from the Window DC.
+//	hbmScreen = CreateCompatibleBitmap(hdcWindow, FrameWidth, FrameHeight);
+//
+//	// Select the compatible bitmap into the compatible memory DC.
+//	SelectObject(hdcMemDC, hbmScreen);
+//
+//	// Bit block transfer into our compatible memory DC.
+//	BitBlt(hdcMemDC,
+//		0, 0,
+//		FrameWidth, FrameHeight,
+//		hdcWindow,
+//		0, 0,
+//		SRCCOPY);
+//
+//	// 暂存到剪贴板
+//	OpenClipboard(rdPtr->MainWindowHandle);
+//	EmptyClipboard();
+//	SetClipboardData(CF_BITMAP, hbmScreen);
+//	CloseClipboard();
+//
+//	// Clean up.
+//	DeleteObject(hbmScreen);
+//	DeleteObject(hdcMemDC);
+//	ReleaseDC(rdPtr->FrameWindowHandle, hdcWindow);
+//
+//	//保存与缩放
+//	int Width = CNC_GetIntParameter(rdPtr);
+//	int Height = CNC_GetIntParameter(rdPtr);
+//	bool SaveToFile = CNC_GetIntParameter(rdPtr);
+//	LPCTSTR FilePath = (LPCTSTR)CNC_GetStringParameter(rdPtr);
+//
+//	cSurface img;
+//	LPSURFACE proto = (LPSURFACE)malloc(sizeof(cSurface));
+//	CImageFilterMgr* pImgMgr = rdPtr->rhPtr->rh4.rh4Mv->mvImgFilterMgr;
+//	CImageFilter    pFilter(pImgMgr);
+//
+//	//放弃了直接从HBITMAP获取信息
+//	//BITMAPINFO* bmp = (BITMAPINFO*)malloc(sizeof(BITMAPINFO));
+//	//memset(bmp, 0, sizeof(*bmp));
+//	//bmp->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+//	//GetDIBits(hdcMemDC, hbmScreen, 0, 1, NULL, (BITMAPINFO*)bmp, DIB_RGB_COLORS);
+//
+//	//从剪贴板获取位图信息
+//	OpenClipboard(rdPtr->MainWindowHandle);
+//	BITMAPINFO* bmp = (BITMAPINFO*)GlobalLock(GetClipboardData(CF_DIB));
+//
+//	//Surface获取位图信息
+//	GetSurfacePrototype(&proto, 24, ST_MEMORYWITHDC, SD_DIB);
+//	img.Create(FrameWidth, FrameHeight, proto);
+//	img.LoadImage(bmp, GetDIBBitmap(bmp));
+//	//img->SetTransparentColor(transCol);
+//
+//	CloseClipboard();
+//
+//	//缩放
+//	cSurface ResizedImg;
+//	ResizedImg.Clone(img);
+//
+//	img.Delete();
+//	img.Create(Width, Height, proto);
+//
+//	ResizedImg.Stretch(img, 0, 0, Width, Height, BMODE_OPAQUE, BOP_COPY, 0, STRF_RESAMPLE);
+//
+//	//输出缩放后的图像到剪贴板
+//	OpenClipboard(rdPtr->MainWindowHandle);
+//	EmptyClipboard();
+//
+//	HGLOBAL cb = GlobalAlloc(GMEM_MOVEABLE, img.GetDIBSize());
+//	BITMAPINFO* OutPut = (BITMAPINFO*)GlobalLock(cb);
+//
+//	img.SaveImage(OutPut, (BYTE*)(OutPut + 1) - 4);
+//	SetClipboardData(CF_DIB, OutPut);
+//
+//	GlobalUnlock(cb);
+//	CloseClipboard();
+//
+//	//保存到文件	
+//	if (!SaveToFile) {
+//		return 0;
+//	}
+//
+//	//获取JPEG格式的FilterID
+//	auto GetFilterID = [pImgMgr]() -> DWORD {
+//		for (int i = 0; i < pImgMgr->GetFilterCount(); i++)
+//		{
+//			if (wcscmp(pImgMgr->GetFilterNameW(i), _T("JPEG")) == 0) {
+//				return pImgMgr->GetFilterID(i);
+//			}
+//		}
+//		return pImgMgr->GetFilterID(0);
+//	};
+//
+//	ExportImage(pImgMgr, FilePath, &img, GetFilterID());
+//
+//	return 0;
+//}
+
 //截取场景区域到剪贴板
 short WINAPI DLLExport BitBltFrameArea(LPRDATA rdPtr, long param1, long param2) {
-	// 截取
-	// 参考范例：
-	// https://docs.microsoft.com/en-us/windows/win32/gdi/capturing-an-image
-	
-	// Source Area
+	//获取参数
+	int Width = CNC_GetIntParameter(rdPtr);
+	int Height = CNC_GetIntParameter(rdPtr);
+	bool SaveToFile = CNC_GetIntParameter(rdPtr);
+	LPCTSTR FilePath = (LPCTSTR)CNC_GetStringParameter(rdPtr);
+
+	//计算截取区域
 	RECT CurrentFrameRect;
 	::GetWindowRect(rdPtr->FrameWindowHandle, &CurrentFrameRect);
 	int FrameWidth = CurrentFrameRect.right - CurrentFrameRect.left;
 	int FrameHeight = CurrentFrameRect.bottom - CurrentFrameRect.top;
 
-	// Defination
-	HDC hdcWindow;
-	HDC hdcMemDC = NULL;
-	HBITMAP hbmScreen = NULL;	
+	//Surface相关
+	cSurface img;
+	LPSURFACE proto = (LPSURFACE)malloc(sizeof(cSurface));
+	CImageFilterMgr* pImgMgr = rdPtr->rhPtr->rh4.rh4Mv->mvImgFilterMgr;
+	CImageFilter    pFilter(pImgMgr);
 
-	// Retrieve the handle to a display device context.
-	hdcWindow = GetDC(rdPtr->FrameWindowHandle);
+	//Surface获取位图信息
+	GetSurfacePrototype(&proto, 24, ST_MEMORYWITHDC, SD_DIB);
+	img.Create(FrameWidth, FrameHeight, proto);
+	
+	// 截取
+	// 参考范例：
+	// https://docs.microsoft.com/en-us/windows/win32/gdi/capturing-an-image
+	// https://github.dev/clickteam-plugin/Surface 2474
+	
+	HDC hdcWindow = GetDC(rdPtr->FrameWindowHandle);
+	HDC sdc = img.GetDC();
 
-	// Create a compatible DC, which is used in a BitBlt from the window DC.
-	hdcMemDC = CreateCompatibleDC(hdcWindow);
-
-	// Create a compatible bitmap from the Window DC.
-	hbmScreen = CreateCompatibleBitmap(hdcWindow, FrameWidth, FrameHeight);
-
-	// Select the compatible bitmap into the compatible memory DC.
-	SelectObject(hdcMemDC, hbmScreen);
-
-	// Bit block transfer into our compatible memory DC.
-	BitBlt(hdcMemDC,
+	BitBlt(sdc,
 		0, 0,
 		FrameWidth, FrameHeight,
 		hdcWindow,
 		0, 0,
 		SRCCOPY);
 
-	// 暂存到剪贴板
-	OpenClipboard(rdPtr->MainWindowHandle);
-	EmptyClipboard();
-	SetClipboardData(CF_BITMAP, hbmScreen);
-	CloseClipboard();
-
-	// Clean up.
-	DeleteObject(hbmScreen);
-	DeleteObject(hdcMemDC);
-	ReleaseDC(rdPtr->FrameWindowHandle, hdcWindow);		
-
-	//保存与缩放
-	int Width = CNC_GetIntParameter(rdPtr);
-	int Height = CNC_GetIntParameter(rdPtr);
-	bool SaveToFile = CNC_GetIntParameter(rdPtr);
-	LPCTSTR FilePath = (LPCTSTR)CNC_GetStringParameter(rdPtr);
-
-	cSurface img;
-	LPSURFACE proto = (LPSURFACE)malloc(sizeof(cSurface));
-	CImageFilterMgr* pImgMgr = rdPtr->rhPtr->rh4.rh4Mv->mvImgFilterMgr;
-	CImageFilter    pFilter(pImgMgr);
-
-	//放弃了直接从HBITMAP获取信息
-	//BITMAPINFO* bmp = (BITMAPINFO*)malloc(sizeof(BITMAPINFO));
-	//memset(bmp, 0, sizeof(*bmp));
-	//bmp->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	//GetDIBits(hdcMemDC, hbmScreen, 0, 1, NULL, (BITMAPINFO*)bmp, DIB_RGB_COLORS);
-
-	//从剪贴板获取位图信息
-	OpenClipboard(rdPtr->MainWindowHandle);
-	BITMAPINFO* bmp = (BITMAPINFO*)GlobalLock(GetClipboardData(CF_DIB));
-
-	//Surface获取位图信息
-	GetSurfacePrototype(&proto, 24, ST_MEMORYWITHDC, SD_DIB);
-	img.Create(FrameWidth, FrameHeight, proto);	
-	img.LoadImage(bmp, GetDIBBitmap(bmp));
-	//img->SetTransparentColor(transCol);
-
-	CloseClipboard();
+	//清理
+	ReleaseDC(rdPtr->FrameWindowHandle, hdcWindow);	
+	img.ReleaseDC(sdc);
 
 	//缩放
 	cSurface ResizedImg;
@@ -457,19 +543,45 @@ short WINAPI DLLExport BitBltFrameArea(LPRDATA rdPtr, long param1, long param2) 
 	if (!SaveToFile) {
 		return 0;
 	}
+
+	//获取扩展名对应的FilterName
+	auto  GetFilterName = [](LPCWSTR Name)->LPCWSTR {
+		LPCWSTR ExtList[8][2] = {
+			_T(".png"),_T("Portable Network Graphics"),
+			_T(".tga"),_T("Targa Bitmap"),
+			_T(".bmp"),_T("Windows Bitmap"),
+			_T(".jpg"),_T("JPEG"),
+			_T(".flc"),_T("Autodesk FLIC"),
+			_T(".gif"),_T("Compuserve Bitmap"),
+			_T(".avi"),_T("Video For Windows"),
+			_T(".pcx"),_T("PaintBrush")
+		};		
+		for (int i = 0; i < 8; i++) {			
+			if (wcscmp(Name, ExtList[i][0]) == 0) {
+				return ExtList[i][1];
+			}
+		}
+		return _T("Windows Bitmap");
+
+	};
 	
-	//获取JPEG格式的FilterID
-	auto GetFilterID = [pImgMgr]() -> DWORD {
+	//获取扩展名对应的FilterID
+	auto GetFilterID = [pImgMgr, GetFilterName](LPCWSTR Name) -> DWORD {
 		for (int i = 0; i < pImgMgr->GetFilterCount(); i++)
 		{
-			if (wcscmp(pImgMgr->GetFilterNameW(i), _T("JPEG")) == 0) {
+			if (wcscmp(pImgMgr->GetFilterNameW(i), GetFilterName(Name)) == 0) {
 				return pImgMgr->GetFilterID(i);
 			}
 		}
 		return pImgMgr->GetFilterID(0);
 	};
 
-	ExportImage(pImgMgr, FilePath, &img, GetFilterID());
+	//获取扩展名
+	WCHAR* Ext = new WCHAR;
+	_wsplitpath(FilePath, NULL, NULL, NULL, Ext);
+
+	//输出文件
+	ExportImage(pImgMgr, FilePath, &img, GetFilterID(Ext));
 	
 	return 0;
 }
