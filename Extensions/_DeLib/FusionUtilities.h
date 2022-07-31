e20404d0d73f0bd203ea270f5e93dfed7c2f60da
Defisym <55148235+defisym@users.noreply.github.com>
2022-07-31 17:41:28 +0800

    Update FusionUtilities.h

--- Extensions/_DeLib/FusionUtilities.h
+++ Extensions/_DeLib/FusionUtilities.h
@@ -24,6 +24,108 @@ inline bool LPROValid(LPRO object, unsigned long identifier = 0) {
 	return true;		// need not to check identifier
 }
 
+//headerObject 	roHo;		  		// Common structure 
+//
+//rCom			roc;				// Anim/movement structure
+//rMvt			rom;				// Movement structure 
+//rAni			roa;				// Animation structure 
+//rSpr			ros;				// Sprite handling structure
+//rVal			rov;				// Values structure
+
+enum class HeaderName {
+	rCom,
+	rMvt,
+	rAni,
+	rSpr,
+	rVal,
+};
+
+inline size_t GetStructOffset(LPRDATA rdPtr, LPRO object, HeaderName headerName, bool allowRVforCS = false) {
+	if (!LPROValid(object)) {
+		return -1;
+	}
+
+	size_t offset = sizeof(headerObject);
+
+	// Internal objects
+	auto identifier = object->roHo.hoIdentifier;
+
+	if (identifier == IDENTIFIER_LIVES || identifier == IDENTIFIER_SCORE) {
+		return -1;
+	}
+
+	if (identifier == IDENTIFIER_COUNTER || identifier == IDENTIFIER_STRING) {
+		if (allowRVforCS) {
+			offset += sizeof(rCom) + sizeof(rMvt) + sizeof(rSpr);
+
+			return offset;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	if (identifier == IDENTIFIER_ACTIVE) {
+		offset += sizeof(rCom) + sizeof(rMvt) + sizeof(rSpr);
+
+		return offset;
+	}
+
+	// General extensions
+	auto flags = object->roHo.hoOEFlags;
+
+	switch (headerName) {
+	case HeaderName::rCom: {
+		if (!((flags & OEFLAG_MOVEMENTS) || (flags & OEFLAG_ANIMATIONS) || (flags & OEFLAG_SPRITES))) {
+			return -1;
+		}
+
+		break;
+	}
+	case HeaderName::rMvt: {
+		if (!(flags & OEFLAG_MOVEMENTS)) {
+			return -1;
+		}
+
+		break;
+	}
+	case HeaderName::rAni: {
+		if (!(flags & OEFLAG_ANIMATIONS)) {
+			return -1;
+		}
+
+		break;
+	}
+	case HeaderName::rSpr: {
+		if (!(flags & OEFLAG_SPRITES)) {
+			return -1;
+		}
+
+		break;
+	}
+	case HeaderName::rVal: {
+		if (!(flags & OEFLAG_VALUES)) {
+			return -1;
+		}
+
+		break;
+	}
+	}
+
+	offset += (flags & OEFLAG_MOVEMENTS) || (flags & OEFLAG_ANIMATIONS) || (flags & OEFLAG_SPRITES)
+		? sizeof(rCom) : 0;
+
+	if (headerName == HeaderName::rCom) {
+		return offset - sizeof(rCom);
+	}
+
+	offset += (flags & OEFLAG_MOVEMENTS) ? sizeof(rMvt) : 0;
+	offset += (flags & OEFLAG_ANIMATIONS) ? sizeof(rAni) : 0;
+	offset += (flags & OEFLAG_SPRITES) ? sizeof(rSpr) : 0;
+
+	return offset;
+}
+
 // Update edit data
 
 // Usage: in General.cpp
