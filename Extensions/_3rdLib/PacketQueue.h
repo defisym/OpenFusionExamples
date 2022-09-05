dd1ac6decebdb0502939c6f66ec8fb1cba0afcc0
Defisym <55148235+defisym@users.noreply.github.com>
2022-09-06 00:02:07 +0800

    Update PacketQueue.h

--- Extensions/_3rdLib/PacketQueue.h
+++ Extensions/_3rdLib/PacketQueue.h
@@ -1,17 +1,22 @@
 #pragma once
 
-// https://blog.csdn.net/fengchu3415/article/details/82453808
-
-#include <condition_variable>
-#include <functional>
-#include <atomic>
-#include <memory>
-#include <mutex>
 #include <queue>
 
 extern "C" {
 #include <libavformat/avformat.h>
 }
+
+//#define STL
+#define SDL
+
+// https://blog.csdn.net/fengchu3415/article/details/82453808
+
+#ifdef STL
+#include <condition_variable>
+#include <functional>
+#include <atomic>
+#include <memory>
+#include <mutex>
 
 class packetQueue {
 private:
@@ -108,3 +113,100 @@ public:
 		_fullQue.notify_all();
 	}
 };
+#endif
+
+#ifdef SDL
+
+#include <SDL.h>
+#include <SDL_thread.h>
+
+class packetQueue {
+private:
+	std::queue<AVPacket> queue;
+	int dataSize = 0;
+
+	SDL_mutex* mutex;
+	SDL_cond* cond;
+
+public:
+	packetQueue() {
+		mutex = SDL_CreateMutex();
+		cond = SDL_CreateCond();
+	}
+
+	~packetQueue() {
+		SDL_DestroyMutex(mutex);
+		SDL_CondSignal(cond);
+	}
+
+	inline size_t size() {
+		return queue.size();
+	}
+
+	inline int getDataSize() {
+		return dataSize;
+	}
+
+	inline bool put(const AVPacket* pPacket) {
+		AVPacket pkt;
+
+		if (av_packet_ref(&pkt, pPacket) < 0) {
+			return false;
+		}
+
+		SDL_LockMutex(mutex);
+		queue.push(pkt);
+
+		dataSize += pkt.size;
+		//nb_packets++;
+
+		SDL_CondSignal(cond);
+		SDL_UnlockMutex(mutex);
+
+		return true;
+	}
+
+	inline bool get(AVPacket* pPacket, bool block = true) {
+		bool ret = false;
+
+		SDL_LockMutex(mutex);
+
+		while (true) {
+			//if (quit) {
+			//	SDL_UnlockMutex(mutex);
+			//	ret = false;
+
+			//	break;
+			//}
+
+			if (!queue.empty()) {
+				if (av_packet_ref(pPacket, &queue.front()) < 0) {
+					ret = false;
+
+					break;
+				}
+
+				queue.pop();
+				//nb_packets--;
+				dataSize -= pPacket->size;
+
+				ret = true;
+
+				break;
+			}
+			else if (!block) {
+				ret = false;
+
+				break;
+			}
+			else {
+				SDL_CondWait(cond, mutex);
+			}
+		}
+
+		SDL_UnlockMutex(mutex);
+
+		return ret;
+	}
+};
+#endif
