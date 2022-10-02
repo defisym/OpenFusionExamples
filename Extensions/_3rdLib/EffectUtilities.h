a08b3515f63bae71b16a5587baabd2ab780cf13c
Defisym <55148235+defisym@users.noreply.github.com>
2022-10-02 23:32:45 +0800

    IterateEffect

--- Extensions/_3rdLib/EffectUtilities.h
+++ Extensions/_3rdLib/EffectUtilities.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "EffectEx.h"
 #include "WindowsCommon.h"
@@ -24,4 +25,55 @@ inline CEffectEx* GetEffect(LPRDATA rdPtr) {
 	auto pEP = (CEffectEx*)rdPtr->rs.rsEffectParam;
 
 	return pEP;
+}
+
+struct EffectData {
+	LPCSTR pName = nullptr;
+	LPBYTE pData = 0;
+};
+
+inline void IterateEffect(LPRDATA rdPtr) {
+	auto pData = rdPtr->rHo.hoAdRunHeader->rhApp->m_pEffects;
+
+	if (pData == nullptr) {
+		return;
+	}
+
+	auto pEffect = rdPtr->rHo.hoAdRunHeader->rhApp->m_pEffects;
+
+	auto nEffects = *(int*)pEffect;
+	pEffect += sizeof(int);
+
+	auto effectsOffset = new int[nEffects];
+
+	for (auto i = 0; i < nEffects; i++) {
+		effectsOffset[i] = *(int*)pEffect;
+		pEffect += sizeof(int);
+	}
+
+	for (auto j = 0; j < nEffects; j++) {
+		auto pEH = (EffectHdr*)(pData + effectsOffset[j]);
+
+		auto pName = (LPCSTR)((LPBYTE)pEH + pEH->dwEffectNameOffset);
+		auto pData = ((LPBYTE)pEH + pEH->dwEffectDataOffset);
+
+		if (pEH->dwEffectParamsOffset == 0) {
+			continue;
+		}
+
+		auto pParam = (EffectParamsHdr*)((LPBYTE)pEH + pEH->dwEffectParamsOffset);
+
+		auto pParamType = (LPBYTE)pParam + pParam->dwParamTypesOffset;
+		auto pParamName = (LPBYTE)pParam + pParam->dwParamNamesOffset;
+
+		for (auto k = 0; k < (int)pParam->dwNParams; k++) {
+			auto paramType = *pParamType;
+			pParamType++;
+
+			auto paramName = (LPCSTR)pParamName;
+			pParamName += strlen(paramName) + 1;
+		}
+	}
+
+	delete[] effectsOffset;
 }
