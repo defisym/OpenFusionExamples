d4e87a3058f7d1833e2beb94638e93673c575d8b
Defisym <55148235+defisym@users.noreply.github.com>
2023-04-28 09:17:27 +0800

    optimize `stod`

--- Extensions/_DeLib/StrNum.h
+++ Extensions/_DeLib/StrNum.h
@@ -5,8 +5,7 @@
 #include <cmath>
 #include <GeneralDefinition.h>
 
-enum class StrType
-{
+enum class StrType {
 	NotNum,
 	IsNum,
 	IsFloat,
@@ -14,7 +13,7 @@ enum class StrType
 
 //check if a string is number
 constexpr inline StrType StrIsNumCore(const wchar_t* p) {
-	StrType Type= StrType::IsNum;
+	auto type = StrType::IsNum;
 
 	if (*p == L'-') {
 		++p;
@@ -28,7 +27,7 @@ constexpr inline StrType StrIsNumCore(const wchar_t* p) {
 	}
 
 	if (*p == L'.') {
-		Type = StrType::IsFloat;
+		type = StrType::IsFloat;
 
 		++p;
 
@@ -39,10 +38,10 @@ constexpr inline StrType StrIsNumCore(const wchar_t* p) {
 	}
 
 	if (*p != L'\0') {
-		Type = StrType::NotNum;
+		type = StrType::NotNum;
 	}
 
-	return Type;
+	return type;
 }
 
 constexpr inline StrType StrIsNumCore(const std::wstring& p) {
@@ -67,75 +66,92 @@ constexpr inline bool StrIsFloat(const std::wstring& p) {
 	return StrIsFloat(p.c_str());
 }
 
-//convert double to string, 2X faster than to_string
-inline void _copy_str(const std::wstring& src, wchar_t** ppDst){
-	auto size = src.length();
-	auto bufsz = size + 1;
+inline void _copy_str(const std::wstring& src, wchar_t** ppDst) {
+	const auto size = src.length();
+	const auto bufsz = size + 1;
 
 	*ppDst = new wchar_t[bufsz];
 	memset(*ppDst, 0, bufsz);
 
-	memcpy(*ppDst, &src[0], size);
+	memcpy(*ppDst, src.data(), size);
+}
+
+template<typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
+template<typename Arithmetic>
+//convert double to string, 2X faster than to_string
+inline std::wstring ntos(Arithmetic Val) {
+	return std::format(L"{}", Val);
 }
 
 inline std::wstring _itos(int Val) {
-	return std::format(L"{}", Val);
+	return ntos(Val);
+}
+
+inline std::wstring _itos(size_t Val) {
+	return ntos(Val);
 }
 
 inline std::wstring _dtos(double Val) {
-	return std::format(L"{}", Val);
+	return ntos(Val);
 }
 
 inline std::wstring _ftos(float Val) {
-	return std::format(L"{}", Val);
+	return ntos(Val);
 }
 
 inline void _dtos(double Val, std::wstring& Str) {
-	Str = std::format(L"{}", Val);
+	Str = ntos(Val);
 }
 
 inline void _ftos(float Val, std::wstring& Str) {
-	Str = std::format(L"{}", Val);
+	Str = ntos(Val);
 }
 
+
+template<typename Arithmetic>
 //Same, but with sign
-inline std::wstring _dtos_signed(double Val) {
+inline std::wstring ntos_signed(Arithmetic Val) {
 	return std::format(L"{:+}", Val);
+}
+inline std::wstring _dtos_signed(double Val) {
+	return ntos_signed(Val);
 }
 
 inline std::wstring _ftos_signed(float Val) {
-	return std::format(L"{:+}", Val);
+	return ntos_signed(Val);
 }
 
 inline void _dtos_signed(double Val, std::wstring& Str) {
-	Str = std::format(L"{:+}", Val);	
+	Str = ntos_signed(Val);
 }
 
 inline void _dtos_signed(double Val, wchar_t** Str) {
-	_copy_str(_dtos_signed(Val), Str);	
+	_copy_str(_dtos_signed(Val), Str);
 }
 
-inline void _ftos_signed(double Val, std::wstring& Str) {
+inline void _ftos_signed(float Val, std::wstring& Str) {
 	_dtos_signed(Val, Str);
 }
 
-inline void _ftos_signed(double Val, wchar_t** Str) {
+inline void _ftos_signed(float Val, wchar_t** Str) {
 	_dtos_signed(Val, Str);
 }
 
 inline std::wstring _dtos_signed_s(double Val, size_t SpaceNum = 1) {
-	auto positive = Val >= 0;
+	const auto positive = Val >= 0;
 	auto sign = positive ? L'+' : L'-';
 
 	auto ret = _dtos(abs(Val));
 	auto totalLength = SpaceNum + ret.length();
 
-	auto fmt = std::format(L"{}{{:>{}}}", sign, totalLength);
+	const auto fmt = std::format(L"{}{{:>{}}}", sign, totalLength);
 
-	return std::vformat(std::wstring_view(fmt),	std::make_wformat_args(ret));
+	return std::vformat(std::wstring_view(fmt), std::make_wformat_args(ret));
 }
 
-inline std::wstring _ftos_signed_s(double Val, size_t SpaceNum = 1) {
+inline std::wstring _ftos_signed_s(float Val, size_t SpaceNum = 1) {
 	_dtos_signed_s(Val, SpaceNum);
 }
 
@@ -143,71 +159,39 @@ inline void _dtos_signed_s(double Val, wchar_t** Str, size_t SpaceNum = 1) {
 	_copy_str(_dtos_signed_s(Val, SpaceNum), Str);
 }
 
-inline void _ftos_signed_s(double Val, wchar_t** Str, size_t SpaceNum = 1) {
+inline void _ftos_signed_s(float Val, wchar_t** Str, size_t SpaceNum = 1) {
 	_dtos_signed_s(Val, Str, SpaceNum);
 }
 
+constexpr double _get_fracture(int n) {
+	switch (n) {
+	case 1:
+		return  1.0 / 10.0;
+	case 2:
+		return  1.0 / 100.0;
+	case 3:
+		return  1.0 / 1000.0;
+	case 4:
+		return  1.0 / 10000.0;
+	case 5:
+		return  1.0 / 100000.0;
+	case 6:
+		return  1.0 / 1000000.0;
+	case 7:
+		return  1.0 / 10000000.0;
+	case 8:
+		return  1.0 / 100000000.0;
+	case 9:
+		return  1.0 / 1000000000.0;
+	default:
+		return 1.0 / std::pow(10.0, n);
+	}
+}
+
+template<typename Arithmetic>
 //convert string to double, 5X faster than std::stod
-constexpr inline double _stod(const wchar_t* p) {
-	double r = 0.0;
-	bool neg = false;
-
-	if (*p == L'-') {
-		neg = true;
-		++p;
-	}
-	if (*p == L'+') {
-		++p;
-	}
-
-	while (*p >= L'0' && *p <= L'9') {
-		r = (r * 10.0) + (*p - L'0');
-		++p;
-	}
-
-	if (*p == L'.') {
-		double f = 0.0;
-		int n = 0;
-		++p;
-
-		while (*p >= L'0' && *p <= L'9') {
-			f = (f * 10.0) + (*p - L'0');
-			++p;
-			++n;
-		}
-
-		r += f / std::pow(10.0, n);
-	}
-
-	//if (*p != L'\0') {
-	//	return 0.0;
-	//}
-
-	if (neg) {
-		r = -r;
-	}
-
-	return r;
-}
-
-constexpr inline double _stod(const std::wstring& p) {
-	return _stod(p.c_str());
-}
-
-constexpr inline double _stod(const std::wstring_view& str) {
-	return _stod(GetTrimmedStr(const_cast<wchar_t*>(str.data()), str.size()).data());
-}
-
-constexpr inline float _stof(const std::wstring& p) {
-	return (float)_stod(p.c_str());
-}
-
-constexpr inline float _stof(const std::wstring_view& str) {
-	return (float)_stod(str);
-}
-
-constexpr inline int _stoi(const wchar_t* p) {
-	int r = 0;
+constexpr inline Arithmetic ston(const wchar_t* p) {
+	Arithmetic r = 0;
 	bool neg = false;
 
 	if (*p == L'-') {
@@ -223,11 +207,55 @@ constexpr inline int _stoi(const wchar_t* p) {
 		++p;
 	}
 
+	if constexpr (std::is_floating_point_v<Arithmetic>) {
+		if (*p == L'.') {
+			Arithmetic f = 0;
+			int n = 0;
+			++p;
+
+			while (*p >= L'0' && *p <= L'9') {
+				f = (f * 10) + (*p - L'0');
+				++p;
+				++n;
+			}
+
+			r += f * _get_fracture(n);
+		}
+
+		//if (*p != L'\0') {
+		//	return 0;
+		//}
+	}
+
 	if (neg) {
 		r = -r;
 	}
 
 	return r;
+}
+
+constexpr inline double _stod(const wchar_t* p) {
+	return ston<double>(p);
+}
+
+constexpr inline double _stod(const std::wstring& p) {
+	return _stod(p.c_str());
+}
+
+constexpr inline double _stod(const std::wstring_view& str) {
+	return _stod(GetTrimmedStr(const_cast<wchar_t*>(str.data()), str.size()).data());
+}
+
+constexpr inline float _stof(const std::wstring& p) {
+	return (float)_stod(p);
+}
+
+constexpr inline float _stof(const std::wstring_view& str) {
+	return (float)_stod(str);
+}
+
+constexpr inline int _stoi(const wchar_t* p) {
+	return ston<int>(p);
 }
 
 constexpr inline int _stoi(const std::wstring& p) {
@@ -241,84 +269,89 @@ constexpr inline  int _stoi(const std::wstring_view& str) {
 //convert Hex to Dex
 constexpr inline int _hexSheet(const wchar_t p) {
 	switch (p) {
-	//numbers
-	case L'0': {
+		//numbers
+	case L'0':
+	{
 		return 0;
 	}
-	case L'1': {
+	case L'1':
+	{
 		return 1;
 	}
-	case L'2': {
+	case L'2':
+	{
 		return 2;
 	}
-	case L'3': {
+	case L'3':
+	{
 		return 3;
 	}
-	case L'4': {
+	case L'4':
+	{
 		return 4;
 	}
-	case L'5': {
+	case L'5':
+	{
 		return 5;
 	}
-	case L'6': {
+	case L'6':
+	{
 		return 6;
 	}
-	case L'7': {
+	case L'7':
+	{
 		return 7;
 	}
-	case L'8': {
+	case L'8':
+	{
 		return 8;
 	}
-	case L'9': {
+	case L'9':
+	{
 		return 9;
 	}
 	//char
-	case L'A': {
+	case L'A':
+	case L'a':
+	{
 		return 10;
 	}
-	case L'a': {
-		return 10;
-	}
-	case L'B': {
+	case L'B':
+	case L'b':
+	{
 		return 11;
 	}
-	case L'b': {
-		return 11;
-	}
-	case L'C': {
+	case L'C':
+	case L'c':
+	{
 		return 12;
 	}
-	case L'c': {
-		return 12;
-	}
-	case L'D': {
+	case L'D':
+	case L'd':
+	{
 		return 13;
 	}
-	case L'd': {
-		return 13;
-	}
-	case L'E': {
+	case L'E':
+	case L'e':
+	{
 		return 14;
 	}
-	case L'e': {
-		return 14;
-	}
-	case L'F': {
-		return 15;
-	}
-	case L'f': {
+	case L'F':
+	case L'f':
+	{
 		return 15;
 	}
 
-	 //Error
-	default: {
+	//Error
+	default:
+	{
 		return -1;
 	}
 	}
 }
 
 constexpr inline DWORD _h2d(const wchar_t* p, size_t strLen = -1) {
-	auto len = strLen == -1 ? wcslen(p) : strLen;
+	auto len = strLen == static_cast<size_t>(-1) ? wcslen(p) : strLen;
 
 	if (*p == L'#') {
 		p += 1;
@@ -327,12 +360,12 @@ constexpr inline DWORD _h2d(const wchar_t* p, size_t strLen = -1) {
 	if ((*p == L'0') && (*(p + 1) == L'x' || *(p + 1) == L'X')) {
 		p += 2;
 		len -= 2;
-	}	
-	
+	}
+
 	DWORD ret = 0;
 
 	for (DWORD pow = 1; len != 0; pow = pow << 4, len--) {
-		auto conv = _hexSheet(*(p + len - 1));
+		const auto conv = _hexSheet(*(p + len - 1));
 
 		if (conv == -1) {
 			break;
