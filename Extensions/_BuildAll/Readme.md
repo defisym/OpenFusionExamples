d8818ffa635a3e4568a9d00e0e6dc529512c6242
Defisym <defisym@outlook.com>
2023-12-11 11:18:18 +0800

    update build tool
    allow to set config, target and platform by command line

---
+++ Extensions/_BuildAll/Readme.md
@@ -0,0 +1,26 @@
+# BuildAll
+
+## Description
+
+this script will search its parent folder, if any folder there contains sln with the same name of folder, script will
+build it.
+
+## Params
+
+| Command    | Info                                                             |
+| --configs  | config to build, default is `Release Unicode` `Run_Only Unicode` |
+| --target   | target to build, default is `rebuild`                            |
+| --platform | platform to build, default is `Win32`                            |
+
+e.g., you only want to run the post build event, you can use:
+
+```
+|------------|------------------------------------------------------------------|
+Python ./main.py --configs "Release Unicode" "Debug" --target PostBuildEvent
+```
+
+## References
+
+- https://learn.microsoft.com/zh-cn/cpp/build/walkthrough-using-msbuild-to-create-a-visual-cpp-project?view=msvc-170
+- https://learn.microsoft.com/zh-cn/visualstudio/msbuild/msbuild-command-line-reference?view=vs-2022
+- https://learn.microsoft.com/zh-cn/visualstudio/msbuild/msbuild-targets?view=vs-2022
--- Extensions/_BuildAll/main.py
+++ Extensions/_BuildAll/main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import subprocess
 
@@ -8,6 +9,28 @@ scriptPath = os.path.split(__file__)[0]
 projectPath = os.path.abspath(scriptPath + '\\..\\') + '\\'
 
 init(autoreset=True)
+
+parser = argparse.ArgumentParser(description='Build all extensions')
+
+# https://stackoverflow.com/questions/15753701/how-can-i-pass-a-list-as-a-command-line-argument-with-argparse
+# configs, e.g., debug / release
+parser.add_argument('--configs', nargs='+')
+
+# https://learn.microsoft.com/zh-cn/visualstudio/msbuild/msbuild-targets?view=vs-2022
+# target, e.g., build, rebuild
+parser.add_argument('--target')
+parser.add_argument('--platform')
+
+args = parser.parse_args()
+
+configs = args.configs
+configs = configs if configs is not None else ['Release Unicode', 'Run_Only Unicode']
+
+target = args.target
+target = target if target is not None else 'rebuild'
+
+platform = args.platform
+platform = platform if platform is not None else 'Win32'
 
 if __name__ == '__main__':
     print(Fore.LIGHTGREEN_EX + '============================')
@@ -24,15 +47,17 @@ if __name__ == '__main__':
 
             result = []
             if os.path.exists(vcxprojPath):
-                status = subprocess.call(
-                    '{} -p:configuration="Release Unicode" -p:platform="Win32" -t:rebuild "{}"'.format(MSBuild,
-                                                                                                       vcxprojPath))
-                result.append(['Release Unicode', resultInfo(status), status])
+                print(Fore.LIGHTCYAN_EX + 'building project {}'.format(vcxprojPath))
 
-                status = subprocess.call(
-                    '{} -p:configuration="Run_Only Unicode" -p:platform="Win32" -t:rebuild "{}"'.format(MSBuild,
-                                                                                                        vcxprojPath))
-                result.append(['Run_Only Unicode', resultInfo(status), status])
+                for config in configs:
+                    print(Fore.LIGHTBLUE_EX, 'building config {}...'.format(config))
+                    # command line help
+                    # https://learn.microsoft.com/zh-cn/cpp/build/walkthrough-using-msbuild-to-create-a-visual-cpp-project?view=msvc-170
+                    # https://learn.microsoft.com/zh-cn/visualstudio/msbuild/msbuild-command-line-reference?view=vs-2022
+                    status = subprocess.call(
+                        '{} -p:configuration="{}" -p:platform="{}" -t:{} "{}"'.format(MSBuild, config, platform, target,
+                                                                                      vcxprojPath))
+                    result.append([config, resultInfo(status), status])
 
                 summary.append([d, result])
 
@@ -53,4 +78,4 @@ if __name__ == '__main__':
             print(fore + '    Build {} {}'.format(buildType, buildResult))
 
     print(
-        'Build {} projects, {} configs, {} success, {} failed'.format(len(summary), 2 * len(summary), success, failed))
+        'Build {} projects, {} configs, {} success, {} failed'.format(len(summary), len(configs), success, failed))
