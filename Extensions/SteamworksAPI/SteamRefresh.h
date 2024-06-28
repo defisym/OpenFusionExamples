66ff13551212d512391a5bec30da5aba2186c4fb
Defisym <defisym@outlook.com>
2024-06-28 18:48:34 +0800

    custom refresh

--- Extensions/SteamworksAPI/SteamRefresh.h
+++ Extensions/SteamworksAPI/SteamRefresh.h
@@ -6,16 +6,25 @@ class SteamRefreshClass {
 public:
 	enum class RefreshType :unsigned int {
 		None = 0,
+		Custom,
 		AchievementAndStat,
 	};
 
-	using RefreshTasks = std::vector<RefreshType>;
+	struct RefreshTask {
+		RefreshType type = RefreshType::None;
+		SteamRefreshClass* pClass = nullptr;
+
+		bool operator==(const RefreshTask& task) const {
+			return this->type == task.type && this->pClass == task.pClass;
+		}
+	};
+	using RefreshTasks = std::vector<RefreshTask>;
 
 private:
 	// copy only, do not release
 	RefreshTasks* pTasks = nullptr;
 
-	static inline bool UniquePush(RefreshTasks* pTasks, const RefreshType task) {
+	static inline bool UniquePush(RefreshTasks* pTasks, const RefreshTask task) {
 		if (std::ranges::find(*pTasks, task) == pTasks->end()) {
 			pTasks->push_back(task);
 
@@ -29,27 +38,35 @@ public:
 	explicit SteamRefreshClass(RefreshTasks* pTasks) {
 		this->pTasks = pTasks;
 	}
+	virtual ~SteamRefreshClass() = default;
 
 	// Add task then refresh it in handle rountine
-	inline void AddToRefresh(RefreshType type) const {
+	inline void AddToRefresh(const RefreshType type) {
 		if (pTasks == nullptr) { return; }
 		if (type == RefreshType::None) { return; }
 
-		UniquePush(pTasks, type);
+		UniquePush(pTasks, { type,this });
 	}
 
+	// refresh class itself
+	virtual inline void Refresh() {}
 	// handle refresh
 	// tasks added will be removed once refresh is done
 	static inline void Refresh(RefreshTasks* pTasks) {
-		for (const auto& it : *pTasks) {
-			switch (it) {
+		for (const auto& [type, pChild] : *pTasks) {
+			switch (type) {
+			case RefreshType::None:
+				continue;
+			case RefreshType::Custom:
+			{
+				pChild->Refresh();
+				break;
+			}
 			case RefreshType::AchievementAndStat:
 			{
 				SteamUserStats()->StoreStats();
 				break;
 			}
-			case RefreshType::None:
-				continue;
 			}
 		}
 
