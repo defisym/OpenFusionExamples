eef79a64540ba6bbdcc357a19a52dcd12b93ca9f
Defisym <defisym@outlook.com>
2024-10-10 17:57:00 +0800

    fix build error dur to typo

--- Extensions/_DeLib/StringTraits.h
+++ Extensions/_DeLib/StringTraits.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <string_view>
 
+#include <type_traits>
+
 template<typename T>
 concept StringConcept = std::is_same_v<std::remove_cvref_t<T>, std::string>
 || std::is_same_v<std::remove_cvref_t<T>, std::wstring>;
@@ -12,7 +14,7 @@ struct CharType {
     using Type = std::remove_cvref_t<decltype(StringType{}[0]) > ;
 
     static size_t Length(const Type* p) {
-        if constexpr (std::is_same_v<std::remove_cvref<StringType>, std::wstring>) {
+        if constexpr (std::is_same_v<std::remove_cvref_t<StringType>, std::wstring>) {
             return wcslen(p);
         }
         else {
@@ -35,12 +37,16 @@ struct ViewType<std::wstring> {
 };
 
 template <typename T>
-concept CStyleWideStrConcept = std::is_same_v<std::remove_cv_t<T>, wchar_t*>
-|| std::is_same_v<std::remove_cv_t<T>, const wchar_t*>;
+concept CStyleWideStrConcept = std::is_same_v<std::remove_cvref_t<T>, wchar_t*>
+|| std::is_same_v<std::remove_cvref_t<T>, const wchar_t*>;
 
 template <typename T>
-concept CStyleCharStrConcept = std::is_same_v<std::remove_cv_t<T>, char*>
-|| std::is_same_v<std::remove_cv_t<T>, const char*>;
+concept CStyleCharStrConcept = std::is_same_v<std::remove_cvref_t<T>, char*>
+|| std::is_same_v<std::remove_cvref_t<T>, const char*>;
 
 template <typename T>
 concept CStyleStrConcept = CStyleWideStrConcept<T> || CStyleCharStrConcept<T>;
+
+template<typename T>
+concept CharConcept = std::is_same_v<std::remove_cvref_t<T>, char>
+|| std::is_same_v<std::remove_cvref_t<T>, wchar_t>;
