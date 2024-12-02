cdb3b590273df12842ecb07c65dd9aec16195183
Defisym <defisym@outlook.com>
2024-12-02 17:00:15 +0800

    Update LockHelper.h

--- Extensions/_DeLib/LockHelper.h
+++ Extensions/_DeLib/LockHelper.h
@@ -1,16 +1,52 @@
 #pragma once
 
+// ------------------------------------------------
+// Include
+// ------------------------------------------------
+
 #include <SDL_mutex.h>
 #include <SDL_atomic.h>
 #pragma comment(lib, "SDL2.lib")
 
 #include <type_traits>
 
+// ------------------------------------------------
+// Concept
+// ------------------------------------------------
+
 template<typename T>
 concept LockConcept = std::is_same_v<std::remove_cvref_t<T>, SDL_SpinLock>
 || std::is_same_v<std::remove_cvref_t<T>, SDL_mutex>;
 
-template<LockConcept Lock>
+// ------------------------------------------------
+// Lock
+// ------------------------------------------------
+
+template<LockConcept LockType>
+struct Lock;
+
+template<>
+struct Lock<SDL_SpinLock> {
+    SDL_SpinLock lock = 0;
+
+    SDL_SpinLock* GetLock() { return &lock; }
+};
+
+template<>
+struct Lock<SDL_mutex> {
+    SDL_mutex* plock = nullptr;
+
+    Lock() { plock = SDL_CreateMutex(); }
+    ~Lock() { SDL_DestroyMutex(plock); }
+
+    SDL_mutex* GetLock() { return plock; }
+};
+
+// ------------------------------------------------
+// LockHelper
+// ------------------------------------------------
+
+template<LockConcept LockType>
 struct LockHelper;
 
 template<>
@@ -21,6 +57,8 @@ public:
     explicit LockHelper(SDL_SpinLock* plock) :_plock(plock) {
         SDL_AtomicLock(plock);
     }
+    explicit LockHelper(Lock<SDL_SpinLock>& lock)
+        :LockHelper(lock.GetLock()) { }
     ~LockHelper() {
         SDL_AtomicUnlock(_plock);
     }
@@ -36,9 +74,29 @@ public:
     explicit LockHelper(SDL_mutex* plock) :_plock(plock) {
         SDL_LockMutex(plock);
     }
+    explicit LockHelper(Lock<SDL_mutex>& lock)
+        :LockHelper(lock.GetLock()) {
+    }
     ~LockHelper() {
         SDL_UnlockMutex(_plock);
     }
 };
 
 using MutexLockHelper = LockHelper<SDL_mutex>;
+
+// ------------------------------------------------
+// Get LockHelper
+// ------------------------------------------------
+
+template<LockConcept LockType>
+struct GetLockHelper;
+
+template<>
+struct GetLockHelper<SDL_SpinLock> {
+    using Type = SpinLockHelper;
+};
+
+template<>
+struct GetLockHelper<SDL_mutex> {
+    using Type = MutexLockHelper;
+};
