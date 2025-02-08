d2fc1181371768347632eb8f6ec62f295059abc4
Defisym <55148235+defisym@users.noreply.github.com>
2025-02-08 22:02:39 +0800

    fix build error due to `min`/`max` macro
    Not sure why the same code can build on another PC, and that's why those code remains untouched

--- Extensions/WinAPI/Main.cpp
+++ Extensions/WinAPI/Main.cpp
@@ -411,7 +411,7 @@ long WINAPI DLLExport IsActiveAtTop(LPRDATA rdPtr, long param1, long param2) {
 
 	if (pObj != nullptr && LPROValid(pObj, IDENTIFIER_ACTIVE)) {
 		LPRO pResult = nullptr;
-		int zOrder = std::numeric_limits<int>::min();
+		int zOrder = (std::numeric_limits<int>::min)();
 
 		rdPtr->pSelect->ForEach(rdPtr, oil, [&](LPRO pObject) {
 			if (pObject->ros.rsZOrder >= zOrder) {
@@ -928,7 +928,7 @@ short WINAPI DLLExport RecursiveGaussBlur(LPRDATA rdPtr, long param1, long param
 
 		long p1 = CNC_GetFloatParameter(rdPtr);
 		double sigma = *(float*)&p1;
-		sigma = std::min(MAX_SIGMA, std::max(MIN_SIGMA, sigma));
+		sigma = (std::min)(MAX_SIGMA, (std::max)(MIN_SIGMA, sigma));
 
 		long p2 = CNC_GetFloatParameter(rdPtr);
 		float scale = *(float*)&p2;
@@ -1120,7 +1120,7 @@ short WINAPI DLLExport MultiThreadGaussBlur(LPRDATA rdPtr, long param1, long par
 		constexpr auto GB_MAX_RADIUS = 65535;
 
 		int radius = CNC_GetIntParameter(rdPtr);
-		radius = std::min(GB_MAX_RADIUS, std::max(GB_MIN_RADIUS, radius));
+		radius = (std::min)(GB_MAX_RADIUS, (std::max)(GB_MIN_RADIUS, radius));
 
 		float sigma = (float)(radius - 0.5) / 3;
 
@@ -1692,7 +1692,7 @@ long WINAPI DLLExport GetFileListSize(LPRDATA rdPtr, long param1) {
 
 long WINAPI DLLExport GetFileListAt(LPRDATA rdPtr, long param1) {
 	size_t Pos = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
-	Pos = std::max(0u, std::min(Pos, rdPtr->FileList->size() - 1));
+	Pos = (std::max)(0u, (std::min)(Pos, rdPtr->FileList->size() - 1));
 
 	NewStr(rdPtr->FileListOutPut, rdPtr->FileList->at(Pos));
 
