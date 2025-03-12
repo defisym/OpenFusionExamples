0f01dab5a3a395379e277ae6f65034cf253801d1
Defisym <defisym@outlook.com>
2025-03-12 20:49:17 +0800

    Create Adapter.h

---
+++ Extensions/_3rdLib/D3DUtilities/Adapter.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "DXGI.h"
+
+#include <vector>
+
+class Adapter {
+    DXGI& DXGIRef;
+
+    std::vector<DXGI::ComPtr<IDXGIAdapter3>> pAdapters = {};
+    UINT AdapterOrdinal = 0;
+
+public:
+    inline HRESULT EnumAdapters() {
+        HRESULT hr;
+        DXGI::ComPtr<IDXGIAdapter> pTempAdapter;
+        AdapterOrdinal = 0;
+
+        while (DXGIRef.pDXGIFactory->EnumAdapters(AdapterOrdinal, &pTempAdapter) != DXGI_ERROR_NOT_FOUND) {
+            DXGI::ComPtr<IDXGIAdapter3> pDXGIAdapter = nullptr;
+            hr = pTempAdapter->QueryInterface(IID_PPV_ARGS(&pDXGIAdapter));
+            
+            if (FAILED(hr)) {              
+                return hr;
+            }
+
+            pAdapters.emplace_back();
+            ++AdapterOrdinal;
+        }
+
+        return S_OK;
+    }
+
+    Adapter(DXGI& DXGI) : DXGIRef(DXGI) {
+        if (EnumAdapters() != S_OK) {
+            throw D3DException("Failed to query IDXGIAdapter3 interface from selected adapter.");
+        }
+
+    }
+
+    ~Adapter() = default;
+};
+
