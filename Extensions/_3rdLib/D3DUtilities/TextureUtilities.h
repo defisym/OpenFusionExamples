3f42f7bc35e2fa189454c5757bd14cd7e8e0ee05
Defisym <defisym@outlook.com>
2025-03-14 21:23:11 +0800

    Create TextureUtilities.h

---
+++ Extensions/_3rdLib/D3DUtilities/TextureUtilities.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <d3d11.h>
+
+// fusion surface info use pointer to pointer to hold texture
+inline ID3D11Texture2D* CastPointer(void** ppTexture) {
+    if (ppTexture == nullptr) { return nullptr; }
+    return *reinterpret_cast<ID3D11Texture2D**>(ppTexture);
+}
+
+inline ID3D11Texture2D* CastPointer(void* pTexture) {
+    return reinterpret_cast<ID3D11Texture2D*>(pTexture);
+}
+
+inline D3D11_TEXTURE2D_DESC GetDesc(ID3D11Texture2D* pD3DTexture) {
+    if (pD3DTexture == nullptr) { return {}; }
+    
+    D3D11_TEXTURE2D_DESC desc = {};
+    pD3DTexture->GetDesc(&desc);
+
+    return desc;
+}
+
+// use void* for texture to hide conversion routine
+inline D3D11_TEXTURE2D_DESC GetDesc(void* pTexture) {
+    return GetDesc(CastPointer(pTexture));
+}
