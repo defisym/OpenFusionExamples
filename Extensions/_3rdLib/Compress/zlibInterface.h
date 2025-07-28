f493340850f5314e21d49dd098599a0b7267d1bf
Defisym <defisym@outlook.com>
2025-07-28 18:54:37 +0800

    Update zlibInterface.h
    Optimize APIs

--- Extensions/_3rdLib/Compress/zlibInterface.h
+++ Extensions/_3rdLib/Compress/zlibInterface.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "zlib/zlib.h"
-#include "GeneralDefinition.h"
+#include "GeneralDefinition.h"  // for macro RELATIVE_PATH
 
 #ifdef _WIN64
 #pragma	comment(lib, RELATIVE_PATH("zlibstatic_x64.lib"))
@@ -10,62 +10,92 @@
 #endif
 
 #include <string>
-#include <memory>
 
-constexpr auto ZLIB_COMPRESSFAILED = 0;
-constexpr auto ZLIB_DECOMPRESSFAILED = 1;
-
-//char* buf = nullptr;
-//auto compressSz = ZLIBI_CompressToBuffer(srcStr, buf);
-inline int ZLIBI_CompressToBuffer(const std::string& srcStr, char*& buf, int compressionLevel = Z_DEFAULT_COMPRESSION) {    
-    const auto srcSz = (uLong)srcStr.length();
-    const auto sz = compressBound(srcSz);
-
-    buf = new char [sz];
-    memset(buf, 0, sz);
+inline bool ZLIBI_Compress(const char* pSrc, const size_t srcSz, 
+    std::string& out, const char compressionLevel = Z_DEFAULT_COMPRESSION) {
+    const auto sz = compressBound(static_cast<uLong>(srcSz));
+    out.clear();
+    out.resize(sz, '\0');
 
     uLong compressedSize = sz;
 
-    const auto ret = compress2((Bytef*)buf, &compressedSize, (Bytef*)srcStr.data(), srcSz, compressionLevel);
+    const auto ret = compress2(reinterpret_cast<Bytef*>(out.data()),
+        &compressedSize,
+        reinterpret_cast<const Bytef*>(pSrc),
+        static_cast<uLong>(srcSz),
+        compressionLevel);
 
-    if (ret == Z_OK) {
-        return compressedSize;
-    }
+    if (ret != Z_OK) { compressedSize = 0; }
+    out.resize(compressedSize);
 
-    throw ZLIB_COMPRESSFAILED;
+    return ret == Z_OK;
 }
 
-inline int ZLIBI_CompressToBufferDefault(const std::string& srcStr, char*& buf) {
-    return ZLIBI_CompressToBuffer(srcStr, buf);
+inline bool ZLIBI_Compress(const std::string& in, std::string& out,
+    const char compressionLevel = Z_DEFAULT_COMPRESSION) {
+    return ZLIBI_Compress(in.data(), in.size(), out, compressionLevel);
 }
 
-//auto deCompressStr = ZLIBI_DeCompressToString(buf, compressSz);
-inline std::string ZLIBI_DeCompressToString(const char* srcBuf, const unsigned int bufSz, const unsigned char estimateRatio = 4) {
-    uLong blockSz = !estimateRatio ? (bufSz << 3) : bufSz * estimateRatio;
+// return empty string if failed
+inline std::string ZLIBI_Compress(const char* pSrc, const size_t srcSz,
+    const char compressionLevel = Z_DEFAULT_COMPRESSION) {
+    std::string compressed = {};
+    ZLIBI_Compress(pSrc, srcSz, compressed, compressionLevel);
+
+    return compressed;
+}
+
+inline std::string ZLIBI_Compress(const std::string& in,
+    const char compressionLevel = Z_DEFAULT_COMPRESSION) {
+    return ZLIBI_Compress(in.data(), in.size(), compressionLevel);
+}
+
+// estimateRatio: 
+//      0 -> decompress buffer is 8 times larger than input
+//      other -> decompress buffer is input size * estimateRatio
+inline bool ZLIBI_UnCompress(const char* pSrc, const size_t srcSz,
+    std::string& out, const unsigned char estimateRatio = 4) {
+    size_t blockSz = !estimateRatio ? (srcSz << 3) : srcSz * estimateRatio;
 
     while (true) {
-        std::unique_ptr<char>deComBuf(new char [blockSz + 1]);
-        memset(deComBuf.get(), 0, blockSz + 1);
+        out.clear();
+        out.resize(blockSz, '\0');
+        auto deCompressedSize = static_cast<uLong>(blockSz);
 
-        auto deCompressedSize = blockSz;
-
-        const auto ret = uncompress((Bytef*)deComBuf.get(), &deCompressedSize, (Bytef*)srcBuf, bufSz);
-
-        if (ret == Z_OK) {
-            auto dstStr = std::string(deComBuf.get(), deCompressedSize);
-
-            return dstStr;
-        }
+        const auto ret = uncompress(reinterpret_cast<Bytef*>(out.data()),
+            &deCompressedSize,
+            reinterpret_cast<const Bytef*>(pSrc),
+            static_cast<uLong>(srcSz));
 
         if (ret == Z_BUF_ERROR) {
             blockSz = blockSz << 1;
             continue;
-        }	
+        }
 
-        throw ZLIB_DECOMPRESSFAILED;
+        if (ret != Z_OK) { deCompressedSize = 0; }
+        out.resize(deCompressedSize);
+
+        return ret == Z_OK;
     }
+
+    return false;
 }
 
-inline std::string ZLIBI_DeCompressToStringDefault(const char* srcBuf, const unsigned int bufSz){
-    return ZLIBI_DeCompressToString(srcBuf, bufSz);
+inline bool ZLIBI_UnCompress(const std::string& in, std::string& out,
+    const unsigned char estimateRatio = 4) {
+    return ZLIBI_UnCompress(in.data(), in.size(), out, estimateRatio);
+}
+
+// return empty string if failed
+inline std::string ZLIBI_UnCompress(const char* pSrc, const size_t srcSz,
+    const unsigned char estimateRatio = 4) {
+    std::string uncompressed = {};
+    ZLIBI_UnCompress(pSrc, srcSz, uncompressed, estimateRatio);
+
+    return uncompressed;
+}
+
+inline std::string ZLIBI_UnCompress(const std::string& in,
+    const unsigned char estimateRatio = 4) {
+    return ZLIBI_UnCompress(in.data(), in.size(), estimateRatio);
 }
