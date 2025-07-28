a55a2c9e1466968636e3cccc0be5b6cef290cc4a
Defisym <defisym@outlook.com>
2025-07-28 18:56:54 +0800

    remove non-zlib support

--- Extensions/_3rdLib/Compress/lz4Interface.h
+++ Extensions/_3rdLib/Compress/lz4Interface.h
@@ -13,6 +13,7 @@ constexpr auto LZ4_DECOMPRESSFAILED = 1;
 
 //char* buf = nullptr;
 //auto compressSz = LZ4I_CompressToBuffer(srcStr, buf);
+[[DEPRECATED("Use ZLIBI_Compress instead")]]
 inline int LZ4I_CompressToBuffer(const std::string& srcStr, char*& buf , int compressionLevel = LZ4HC_CLEVEL_DEFAULT) {
 	auto srcSz = (int)srcStr.length();
 	auto sz = LZ4_compressBound(srcSz);	
@@ -29,11 +30,13 @@ inline int LZ4I_CompressToBuffer(const std::string& srcStr, char*& buf , int com
 	return compressedSize;
 }
 
+[[DEPRECATED("Use ZLIBI_Compress instead")]]
 inline int LZ4I_CompressToBufferDefault(const std::string& srcStr, char*& buf) {
 	return LZ4I_CompressToBuffer(srcStr, buf);
 }
 
 //auto deCompressStr = LZ4I_DeCompressToString(buf, compressSz);
+[[DEPRECATED("Use ZLIBI_Compress instead")]]
 inline std::string LZ4I_DeCompressToString(const char* srcBuf, const unsigned int bufSz, const unsigned char estimateRatio = 3) {
 	const auto blockSz = !estimateRatio ? (bufSz << 8) - bufSz - 2526 : bufSz * estimateRatio;
 
@@ -51,6 +54,7 @@ inline std::string LZ4I_DeCompressToString(const char* srcBuf, const unsigned in
 	return dstStr;
 }
 
+[[DEPRECATED("Use ZLIBI_Compress instead")]]
 inline std::string LZ4I_DeCompressToStringDefault(const char* srcBuf, const unsigned int bufSz) {
 	return LZ4I_DeCompressToString(srcBuf, bufSz);
 }
