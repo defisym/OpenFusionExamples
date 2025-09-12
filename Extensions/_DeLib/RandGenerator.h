9e9e90bd3401ff045d040b80f46852f1111ce91b
Defisym <defisym@outlook.com>
2025-09-12 17:48:51 +0800

    add comment for `RandGenerator`

--- Extensions/_DeLib/RandGenerator.h
+++ Extensions/_DeLib/RandGenerator.h
@@ -2,4 +2,11 @@
 
 #include "RandomBase.h"
 
+// -----------------------------------------------
+// Type alias for a random number generator
+// -----------------------------------------------
+
+// Usage:
+// auto rand = RandGenerator(min, max);
+// int value = rand.Generate();
 using RandGenerator = RandomBase<int, std::mt19937, std::uniform_int_distribution<int>>;
