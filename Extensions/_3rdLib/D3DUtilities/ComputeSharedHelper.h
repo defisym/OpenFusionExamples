4b72e81f4598546c037e792b3fd7127231a06b4c
Defisym <defisym@outlook.com>
2026-01-30 01:05:13 +0800

    clear 8 slots by default
    one is not enough sometimes

--- Extensions/_3rdLib/D3DUtilities/ComputeSharedHelper.h
+++ Extensions/_3rdLib/D3DUtilities/ComputeSharedHelper.h
@@ -109,11 +109,11 @@ struct ComputeSharedHelper {
             ThreadGroupCountZ);
 
         // Unbind the input textures from the CS for good housekeeping
-        ID3D11ShaderResourceView* nullSRV[] = { nullptr };
-        pDeviceCtx->CSSetShaderResources(0, 1, nullSRV);
+        ID3D11ShaderResourceView* nullSRV[8] = { };
+        pDeviceCtx->CSSetShaderResources(0, std::size(nullSRV), nullSRV);
 
         // Unbind output from compute shader
-        ID3D11UnorderedAccessView* nullUAV[] = { nullptr };
-        pDeviceCtx->CSSetUnorderedAccessViews(0, 1, nullUAV, 0);
+        ID3D11UnorderedAccessView* nullUAV[8] = { };
+        pDeviceCtx->CSSetUnorderedAccessViews(0, std::size(nullUAV), nullUAV, 0);
     }
 };
